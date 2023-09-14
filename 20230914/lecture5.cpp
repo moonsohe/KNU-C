42ec92587a1cd42d3251fb361705c265572ed472
moonsohe <thgml89567@naver.com>
2023-09-14 10:50:59 +0900

    Update lecture5.cpp

--- 20230914/lecture5.cpp
+++ 20230914/lecture5.cpp
@@ -6,7 +6,7 @@ int main()
 	printf("년도 입력 :");
 	scanf_s("%d", &year);
 
-if((year%4==0 && !(year%100==0)) || year % 400 == 0)
+if((year%4==0 && year%100!=0) || year % 400 == 0)
 	printf("윤년입니다\n");
 
 else
