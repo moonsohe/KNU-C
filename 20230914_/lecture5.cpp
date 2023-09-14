822a3c1c2414aef57a60ad598504a36b320d5d44
moonsohe <thgml89567@naver.com>
2023-09-14 10:50:07 +0900

    year

---
+++ 20230914_/lecture5.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int year, a;
+	printf("년도 입력 :");
+	scanf_s("%d", &year);
+
+	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+		printf("윤년입니다\n");
+
+	else
+		printf("윤년이 아닙니다\n");
+
+	return 0;
+}
