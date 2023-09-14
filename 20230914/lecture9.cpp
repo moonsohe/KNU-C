4bcfb29c45271d43187db33e6c4a508563c160fa
moonsohe <thgml89567@naver.com>
2023-09-14 12:05:02 +0900

    Update lecture9.cpp

--- 20230914/lecture9.cpp
+++ 20230914/lecture9.cpp
@@ -5,15 +5,14 @@ int main()
 	int i;
 	int sum = 0;
 	int val = 1;
-	for (i = 0; i < 10; i++)
-	{
+	for (i = 0; i < 10; i++) {
 		if (i % 2 != 0) {
 			printf("i=%d sum=%d\n", i, sum);
 			sum += i;
 	    }
 	}
-		
-	/* {
+	/*
+	{
 		printf("i = %d val = %d sum = %d\n", i, val, sum);
 		sum += val;
 		val += 1;
