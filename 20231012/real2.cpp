0730d3cf8e60692a5bc10a4fe3ec5c0d5e703938
moonsohe <thgml89567@naver.com>
2023-10-12 11:11:26 +0900

    Create real2.cpp

---
+++ 20231012/real2.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void swap(int* a, int* b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
+
+
+void sort(int *brr) {
+	for (int k = 0; k < 4; k++) {
+		for (int i = 0; i < 4-k; i++) {
+			if (brr[i] > brr[i + 1]) {
+				swap(&brr[i], &brr[i + 1]);
+			}
+		}
+	}
+}
+int main() {
+	int arr[5] = { 1,6,10,3,5 };
+
+	sort(arr);
+	for (int i = 0; i < 5; i++)
+		printf("%d ", arr[i]);
+}
