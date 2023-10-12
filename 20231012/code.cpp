d7e35783e00d3e1a5ab531a4fcfdb1ec85a3f99e
moonsohe <thgml89567@naver.com>
2023-10-12 10:41:39 +0900

    Update code.cpp

--- 20231012/code.cpp
+++ 20231012/code.cpp
@@ -5,9 +5,9 @@ int main()
 	int score[5] = { 87, 92, 94, 65, 33 };
 	int cnt;
 
-	int* p_score = score;
+	int* p_score = score + 1;
 
-	for (cnt = 0; cnt < 5; cnt++) {
+	for (int i = 0; i < 3; i++) {
 		printf("%d\n", *p_score);
 		p_score++;
 	}
