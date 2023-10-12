cc3fb3b824bf49f6a6129966b57f8c4fe255cbc8
moonsohe <thgml89567@naver.com>
2023-10-12 10:14:51 +0900

    Update pointer.cpp

--- 20231012/pointer.cpp
+++ 20231012/pointer.cpp
@@ -4,12 +4,12 @@ int main() {
 	int a = 3;
 	int* ptr1 = &a;
 
-	char b = 'b';
+	char b = "b";
 	int* ptr2 = &b;
 
 	printf("%d\n", ptr1);
-	printf("%d\n", sizeof(ptr1);
+	printf("%d\n", sizeof(ptr1));
 
 	printf("%d\n", ptr2);
-	printf("%d\n", sizeof(ptr2);
+	printf("%d\n", sizeof(ptr2));
 }
