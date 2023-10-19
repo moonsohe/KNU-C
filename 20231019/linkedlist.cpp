9487cbf68e21b74c92718329b68b68b8142ca2fc
moonsohe <thgml89567@naver.com>
2023-10-19 10:41:14 +0900

    Create linkedlist.cpp

---
+++ 20231019/linkedlist.cpp
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct _NODE { 
+	//노드라는 이름이 중복이되면 오류날수도있음 (컴파일러마다다름) _를 써야됨.
+	int data;
+	struct _NODE* next;
+} NODE;
+
+void print_list(NODE* head) {
+	NODE* p = head->next;
+	while (p != NULL) {
+		printf("%d ", p->data);
+		p = p->next;
+	}
+}
+
+void insert_node(NODE* head, int data) {
+	NODE* p = head;
+	while (p->next != NULL) {
+		p = p->next;
+	}
+
+	NODE* new_node = (NODE*)malloc(sizeof(NODE));
+	new_node->data;
+}
+
+int main() {
+	NODE* head = (NODE*)malloc(sizeof(NODE));
+	head -> next = NULL;
+	// (*head). 직접접근 head -> 간접접근
+
+	NODE* n1 = (NODE*)malloc(sizeof(NODE));;
+	n1->data = 1;
+	n1->next = head->next;
+	head->next = n1;
+
+	NODE* n2 = (NODE*)malloc(sizeof(NODE));;
+	n2->data = 2;
+	n2->next = n1->next;
+	head->next = n2;
+
+	NODE* n3 = (NODE*)malloc(sizeof(NODE));;
+	n3->data = 3;
+	n3->next = n1->next;
+	head->next = n3;
+
+	insert_node(head, 4);
+
+	print_list(head);
+
+	return 0;
+}
