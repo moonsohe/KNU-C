5ce280a7404c781abaeef62e7acbd17a5cdb7bd8
moonsohe <thgml89567@naver.com>
2023-10-19 10:14:26 +0900

    Update grade.cpp

--- 20231019/grade.cpp
+++ 20231019/grade.cpp
@@ -1,24 +1,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+struct Student {
+	int sno;
+	char name[10];
+	int score;
+};
 int main() {
+	struct Student* s;
+
 	int student = 0, sum = 0;
 	printf("학생수 입력 : ");
 	scanf_s("%d", &student);
 
 	int* grade;
-	grade = (int*)malloc(student * sizeof(int));
+	s = (struct Student*)malloc(student * sizeof(int));
 	for (int i = 0; i < student; i++) {
+		printf("학생 %d-%d 학번 입력 : ", student, i + 1);
+		scanf_s("%d", &(s[i].sno));
+		printf("학생 %d-%d 이름 입력 : ", student, i + 1);
+		scanf_s("%s", s[i].name, 10);
 		printf("학생 %d-%d 성적 입력 : ", student, i + 1);
-		scanf_s("%d", &grade[i]);
-		sum += grade[i];
+		scanf_s("%d", &(s[i].score));
+		sum += s[i].score;
 	}
 	printf("총점 : %d\n\n", sum);
-	for (int i = 0; i < student; i++)
-		printf("학생 %d-%d 성적 출력 : %d\n", student, i + 1, grade[i]);
+
+	
 	printf("평균 점수 : %.2f", float(sum) / student);
-	free(grade);
-	grade = NULL;
+	free(s);
 
 	return 0;
 
